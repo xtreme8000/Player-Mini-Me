f588f18ef01b583f3b06d83024954d90b5173b99
xtreme8000 <xtreme8000@users.noreply.github.com>
2022-04-01 18:12:54 +0200

    Update for 1.18.2

--- src/main/java/io/github/phoenixvx/paperdoll/ModMenuIntegration.java
+++ src/main/java/io/github/phoenixvx/paperdoll/ModMenuIntegration.java
@@ -2,7 +2,10 @@ package io.github.phoenixvx.paperdoll;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import me.shedaniel.clothconfiglite.api.ConfigScreen;
+
+import me.shedaniel.clothconfig2.api.ConfigBuilder;
+import me.shedaniel.clothconfig2.api.ConfigCategory;
+import me.shedaniel.clothconfig2.api.ConfigEntryBuilder;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.text.TranslatableText;
@@ -12,92 +15,70 @@ import net.minecraft.text.TranslatableText;
  */
 @Environment(EnvType.CLIENT)
 public class ModMenuIntegration implements ModMenuApi {
-    @Override
-    public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return parent -> {
-            ConfigScreen screen = ConfigScreen.create(new TranslatableText("text.paperdoll.title"), parent);
+	@Override
+	public ConfigScreenFactory<?> getModConfigScreenFactory() {
+		return parent -> {
+			ConfigBuilder builder = ConfigBuilder.create().setParentScreen(parent)
+					.setTitle(new TranslatableText("text.paperdoll.title"));
+			ConfigEntryBuilder entryBuilder = builder.entryBuilder();
 
-            screen.add(
-                    new TranslatableText("text.paperdoll.option.render_height"),
-                    PaperDoll.config.render_height,
-                    () -> 50,
-                    o -> {
-                        PaperDoll.config.render_height = (int) o;
-                        PaperDoll.saveConfig();
-                    }
-            );
+			ConfigCategory general = builder.getOrCreateCategory(new TranslatableText("text.paperdoll.general"));
 
-            screen.add(
-                    new TranslatableText("text.paperdoll.option.render_width"),
-                    PaperDoll.config.render_width,
-                    () -> 25,
-                    o -> {
-                        PaperDoll.config.render_width = (int) o;
-                        PaperDoll.saveConfig();
-                    }
-            );
+			general.addEntry(entryBuilder.startIntField(new TranslatableText("text.paperdoll.option.render_height"),
+					PaperDoll.config.render_height).setDefaultValue(50).setSaveConsumer(o -> {
+						PaperDoll.config.render_height = (int) o;
+						PaperDoll.saveConfig();
+					}).build());
 
-            screen.add(
-                    new TranslatableText("text.paperdoll.option.rotation"),
-                    PaperDoll.config.rotation,
-                    () -> 20,
-                    o -> {
-                        PaperDoll.config.rotation = (int) o;
-                        PaperDoll.saveConfig();
-                    }
-            );
+			general.addEntry(entryBuilder.startIntField(new TranslatableText("text.paperdoll.option.render_width"),
+					PaperDoll.config.render_width).setDefaultValue(25).setSaveConsumer(o -> {
+						PaperDoll.config.render_width = (int) o;
+						PaperDoll.saveConfig();
+					}).build());
 
-            screen.add(
-                    new TranslatableText("text.paperdoll.option.dynamic_scale"),
-                    PaperDoll.config.dynamic_scale,
-                    () -> false,
-                    o -> {
-                        PaperDoll.config.dynamic_scale = (boolean) o;
-                        PaperDoll.saveConfig();
-                    }
-            );
+			general.addEntry(entryBuilder
+					.startIntField(new TranslatableText("text.paperdoll.option.rotation"), PaperDoll.config.rotation)
+					.setDefaultValue(20).setSaveConsumer(o -> {
+						PaperDoll.config.rotation = (int) o;
+						PaperDoll.saveConfig();
+					}).build());
 
-            screen.add(
-                    new TranslatableText("text.paperdoll.option.x"),
-                    PaperDoll.config.x,
-                    () -> 10,
-                    o -> {
-                        PaperDoll.config.x = (int) o;
-                        PaperDoll.saveConfig();
-                    }
-            );
+			general.addEntry(
+					entryBuilder.startBooleanToggle(new TranslatableText("text.paperdoll.option.dynamic_scale"),
+							PaperDoll.config.dynamic_scale).setDefaultValue(false).setSaveConsumer(o -> {
+								PaperDoll.config.dynamic_scale = (boolean) o;
+								PaperDoll.saveConfig();
+							}).build());
 
-            screen.add(
-                    new TranslatableText("text.paperdoll.option.y"),
-                    PaperDoll.config.y,
-                    () -> 10,
-                    o -> {
-                        PaperDoll.config.y = (int) o;
-                        PaperDoll.saveConfig();
-                    }
-            );
+			general.addEntry(
+					entryBuilder.startIntField(new TranslatableText("text.paperdoll.option.x"), PaperDoll.config.x)
+							.setDefaultValue(10).setSaveConsumer(o -> {
+								PaperDoll.config.x = (int) o;
+								PaperDoll.saveConfig();
+							}).build());
 
-            screen.add(
-                    new TranslatableText("text.paperdoll.option.only_activity"),
-                    PaperDoll.config.only_activity,
-                    () -> false,
-                    o -> {
-                        PaperDoll.config.only_activity = (boolean) o;
-                        PaperDoll.saveConfig();
-                    }
-            );
+			general.addEntry(
+					entryBuilder.startIntField(new TranslatableText("text.paperdoll.option.y"), PaperDoll.config.y)
+							.setDefaultValue(10).setSaveConsumer(o -> {
+								PaperDoll.config.y = (int) o;
+								PaperDoll.saveConfig();
+							}).build());
 
-            screen.add(
-                    new TranslatableText("text.paperdoll.option.change_swim_fly"),
-                    PaperDoll.config.change_swim_fly,
-                    () -> true,
-                    o -> {
-                        PaperDoll.config.change_swim_fly = (boolean) o;
-                        PaperDoll.saveConfig();
-                    }
-            );
+			general.addEntry(
+					entryBuilder.startBooleanToggle(new TranslatableText("text.paperdoll.option.only_activity"),
+							PaperDoll.config.only_activity).setDefaultValue(false).setSaveConsumer(o -> {
+								PaperDoll.config.only_activity = (boolean) o;
+								PaperDoll.saveConfig();
+							}).build());
 
-            return screen.get();
-        };
-    }
+			general.addEntry(
+					entryBuilder.startBooleanToggle(new TranslatableText("text.paperdoll.option.change_swim_fly"),
+							PaperDoll.config.change_swim_fly).setDefaultValue(true).setSaveConsumer(o -> {
+								PaperDoll.config.change_swim_fly = (boolean) o;
+								PaperDoll.saveConfig();
+							}).build());
+
+			return builder.build();
+		};
+	}
 }
